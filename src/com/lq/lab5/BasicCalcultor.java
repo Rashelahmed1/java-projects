8aa76f5df58c89b669002765b1683767e5813f0b
Rashelahmed1 <rainforest2021@yahoo.com>
2024-02-20 10:32:52 -0500

    Lab 5 : Creating Calculator Classes

---
+++ src/com/lq/lab5/BasicCalcultor.java
@@ -0,0 +1,40 @@
+package com.lq.lab5;
+
+public class BasicCalcultor {
+
+	public double add(double x, double y) {
+		return x + y ;
+		
+	}
+
+
+	public double subtract(double x , double y) {
+		return x - y ;
+		
+	}
+	
+
+	public double multiply(double x , double y) {
+		return x * y ;
+		
+	}
+
+	public double divide(double x , double y) {
+		return x / y ;
+		
+	}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
