5352197e090c8b7e9f158b5bdd88ff8571195f94
Rashelahmed1 <rainforest2021@yahoo.com>
2024-02-20 23:28:53 -0500

    Lab 7 : Creating Calculator Objects

---
+++ src/com/lq/lab7/TrigonometricCalculator.java
@@ -0,0 +1,44 @@
+package com.lq.lab7;
+
+public class TrigonometricCalculator {
+
+	public double sine(double a) {
+		return Math.sin(a);
+	}
+	
+
+	public double cosine(double b) {
+		return Math.cos(b);
+	}
+
+	public double tangent(double c) {
+		return Math.tan(c);
+		
+	}
+	
+	public double arcsine(double d) {
+		return Math.asin(d);
+		
+	}
+	public double arccosine(double e) {
+		return Math.acos(e);
+		
+	}
+	public double arctangent(double f) {
+		return Math.atan(f);
+		
+	}
+
+
+
+
+
+
+
+
+
+
+
+}
+
+	
