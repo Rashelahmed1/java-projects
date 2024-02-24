9ec836f0bd5ffd51fce65bb4d7265b767b79b4fe
Rashelahmed1 <rainforest2021@yahoo.com>
2024-02-24 11:02:47 -0500

    Lab 9 : Creating an inventory system

---
+++ src/com/lq/lab9/ClassicalCD.java
@@ -0,0 +1,76 @@
+package com.lq.lab9;
+
+import java.util.Date;
+
+public class ClassicalCD extends Item{
+
+	private String compuser;
+	
+	String performers [] = new String [5];
+	
+	private String recordingLocation;
+	
+	private Date releseDate;
+
+	public String getCompuser() {
+		return compuser;
+	}
+
+	public void setCompuser(String compuser) {
+		this.compuser = compuser;
+	}
+
+	public String getRecordingLocation() {
+		return recordingLocation;
+	}
+
+	public void setRecordingLocation(String recordingLocation) {
+		this.recordingLocation = recordingLocation;
+	}
+
+	public Date getReleseDate() {
+		return releseDate;
+	}
+
+	public void setReleseDate(Date releseDate) {
+		this.releseDate = releseDate;
+	}
+	
+	private int i = 0;
+
+	public void addPerformer(String performer) {
+		if(i < performers.length) {
+			performers[i] = performer;
+			i ++ ;
+		}
+		else {
+			System.out.println("no more room int the performersArray");
+		}
+	
+	
+	
+	}
+	
+	public void showPerformers() {
+		for(int x = 0 ; x <= i ; x++) {
+			System.out.println(performers[x]);
+		}
+	
+	}
+	
+	
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
