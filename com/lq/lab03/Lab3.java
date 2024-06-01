50161eebfc571db4c61f20f9a437389b8f836c9b
Rashelahmed1 <rainforest2021@yahoo.com>
2024-06-01 13:01:49 -0400

    Declare and print variable
    This commit introduces the following changes:
    
    - Declare String array "daysOfWeek"
    -  Declare String array "monthNames"
    -  Print String array "daysOfWeek"

--- com/lq/lab03/Lab3.java
+++ com/lq/lab03/Lab3.java
@@ -1,5 +1,18 @@
 package com.lq.lab03;
 
 public class Lab3 {
+	public static void main(String[] args) {
+	String daysOfWeek [] = new String [] { "Sun","Mon","Tue","Wed","Thu","Fri","Sat"};
+	
+	String monthNames [] = new String [] {"January","February","March","April","May","June","July",
+			"August","september","Auctober","November","December"};
+	
 
+	for(int i = 0; i < daysOfWeek.length  ; i ++) {
+		System.out.print(daysOfWeek[i]+"\t");
+	}
+
+	System.out.println();
+
+}
 }
