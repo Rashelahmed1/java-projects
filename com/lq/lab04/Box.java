06e10c2feb78ce11ebb05a839519535f4515eb08
Rashelahmed1 <rainforest2021@yahoo.com>
2024-06-04 12:14:27 -0400

    Making ammends for previous  commit message

--- com/lq/lab04/Box.java
+++ com/lq/lab04/Box.java
@@ -2,4 +2,53 @@ package com.lq.lab04;
 
 public class Box {
 
+	private double height;
+	private double width;
+	private double length;
+	
+	
+	
+	public double getHeight() {
+		return height;
+	}
+
+
+
+	public void setHeight(double height) {
+		this.height = height;
+	}
+
+
+
+	public double getWidth() {
+		return width;
+	}
+
+
+
+	public void setWidth(double width) {
+		this.width = width;
+	}
+
+
+
+	public double getLength() {
+		return length;
+	}
+
+
+
+	public void setLength(double length) {
+		this.length = length;
+	}
+
+
+
+	public Box(double height,double width,double length) {
+		
+		setHeight(height);
+		setLength(length);
+		setWidth(width);
+		
+	}
 }
