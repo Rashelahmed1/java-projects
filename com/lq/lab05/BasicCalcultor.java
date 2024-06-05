6454788b7026a4eeb30ac5dd5b2e1bc2ab1b0ce3
Rashelahmed1 <rainforest2021@yahoo.com>
2024-06-05 12:27:35 -0400

    Create new class BasicCalculator
    This commit introduces the following changes:
    
    - create double method add that takes 2 double values and performs addition
    - create double method subtract that takes 2 double values and performs subtraction
    - create double method multiply that takes 2 double values and performs multiplication
    - create double method divide that takes 2 double values and performs division

---
+++ com/lq/lab05/BasicCalcultor.java
@@ -0,0 +1,26 @@
+package com.lq.lab05;
+
+public class BasicCalcultor {
+
+	public double add(double x, double y) {
+		return x + y ;
+		
+	}
+
+
+	public double subtract(double x , double y) {
+		return x - y ;
+		
+	}
+	
+
+	public double multiply(double x , double y) {
+		return x * y ;
+		
+	}
+
+	public double divide(double x , double y) {
+		return x / y ;
+		
+	}
+}
