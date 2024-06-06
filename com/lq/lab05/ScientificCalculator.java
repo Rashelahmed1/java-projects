d219c9d10c51726d278f9cb61f2084a383722373
Rashelahmed1 <rainforest2021@yahoo.com>
2024-06-06 12:20:58 -0400

    Create class ScentificCalculator
    This commit introduces the following changes:
    
    - Declare double static variable PI
    - Declare the property holdValue and create getter getValueFromMemory and setter putValueInMemory
    - Create double method exp that return 0
    - Create double method log that return 0

---
+++ com/lq/lab05/ScientificCalculator.java
@@ -0,0 +1,27 @@
+package com.lq.lab05;
+
+public class ScientificCalculator {
+
+	public static final double PI = 3.14159;
+	private  double holdValue;
+	
+
+
+	public double exp(double x){
+		return 0.0;
+		
+	}
+
+	public double getValueFromMemory() {
+		return holdValue;
+	}
+
+	public void putValueInMemory(double holdValue) {
+		this.holdValue = holdValue;
+	}
+
+	public double log(double y) {
+		return 0.0;
+		
+	}
+}
