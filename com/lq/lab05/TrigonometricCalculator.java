ce3460dbc110a8a99bd37a0880327ec59ed8ad5b
Rashelahmed1 <rainforest2021@yahoo.com>
2024-06-06 12:35:21 -0400

    Create class TrigonometricCalculator
    This commit introduces the following changes:
    
    - Creating double sine method that returns 0
    - Creating double cosine method that returns 0
    - Creating double tangent method that returns 0
    - Creating double arcsine method that returns 0
    - Creating double arccosine method that returns 0
    - Creating double arctangent method that returns 0

---
+++ com/lq/lab05/TrigonometricCalculator.java
@@ -0,0 +1,44 @@
+package com.lq.lab05;
+
+public class TrigonometricCalculator {
+
+	public double sine(double a) {
+		return 0;
+	}
+	
+
+	public double cosine(double b) {
+		return 0;
+	}
+
+	public double tangent(double c) {
+		return 0;
+		
+	}
+	
+	public double arcsine(double d) {
+		return 0;
+		
+	}
+	public double arccosine(double e) {
+		return 0;
+		
+	}
+	public double arctangent(double f) {
+		return 0;
+		
+	}
+
+
+
+
+
+
+
+
+
+
+
+}
+
+	
