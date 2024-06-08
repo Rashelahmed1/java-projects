4cd6f3b27dc23b5ca1a78620e10d9f3261bb88bd
Rashelahmed1 <rainforest2021@yahoo.com>
2024-06-08 10:08:08 -0400

    Creating Box objects
    This commit introduces the following changes:
    
    - Creating ractangle Box object box1
    - Creating cuve Box object box2
    - Printing box1 information about Height, Length,SurfaceArea,Volume,Width.
    - Printing box2 information about Height, Length,SurfaceArea,Volume,Width.

---
+++ com/lq/lab06/BoxDriverMain.java
@@ -0,0 +1,25 @@
+package com.lq.lab06;
+
+import com.lq.lab04.Box;
+
+public class BoxDriverMain {
+
+	public static void main(String[] args) {
+		Box box1 = new Box( 5, 6, 7);
+		Box box2 = new Box (10);
+		
+		System.out.println("Height = " +box1.getHeight());
+		System.out.println("Length = "  +box1.getLength());
+		System.out.println("SurfaceArea = " +box1.getSurfaceArea());
+		System.out.println("Volume = " +box1.getVolume());
+		System.out.println("Width = "  +box1.getWidth());
+		System.out.println("---------------");
+		System.out.println("Height = " +box2.getHeight());
+		System.out.println("Length = "  +box2.getLength());
+		System.out.println("SurfaceArea = " +box2.getSurfaceArea());
+		System.out.println("Volume = " +box2.getVolume());
+		System.out.println("Width = "  +box2.getWidth());
+		System.out.println("---------------");
+	}
+
+}
