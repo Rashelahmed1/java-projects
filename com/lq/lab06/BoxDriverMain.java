b9b5d6c13299806b884d8fca7a74201d58ad1764
Rashelahmed1 <rainforest2021@yahoo.com>
2024-06-08 10:18:32 -0400

    Changing values for box1
    This commit introduces the following changes:
    
    - set box1 length, width, height
    - print new box1 information about Height, Length, SurfaceArea, Volume, Width.
    - Use printBox method to print information about box1
    - Use printBox method to print information about box2

--- com/lq/lab06/BoxDriverMain.java
+++ com/lq/lab06/BoxDriverMain.java
@@ -20,6 +20,32 @@ public class BoxDriverMain {
 		System.out.println("Volume = " +box2.getVolume());
 		System.out.println("Width = "  +box2.getWidth());
 		System.out.println("---------------");
+	
+		
+//		box1.PrintBox();
+//		System.out.println("--------------");
+//		box2.PrintBox();
+//		System.out.println("--------------");
+
+	
+	box1.setLength(3);
+	box1.setWidth(4);
+	box1.setHeight(5);
+	
+
+	System.out.println("Height = " +box1.getHeight());
+	System.out.println("Length = "  +box1.getLength());
+	System.out.println("SurfaceArea = " +box1.getSurfaceArea());
+	System.out.println("Volume = " +box1.getVolume());
+	System.out.println("Width = "  +box1.getWidth());
+	System.out.println("--------------");
+	
+	
+	box1.PrintBox();
+	System.out.println("--------------");
+	box2.PrintBox();
+	
+	
 	}
 
 }
