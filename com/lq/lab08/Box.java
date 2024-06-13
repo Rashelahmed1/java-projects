5ec4d1de8300fb4406dfe85a3bbc2c5368d1d857
Rashelahmed1 <rainforest2021@yahoo.com>
2024-06-13 10:33:09 -0400

    create Box class
    This commit introduces the following changes:
    
    - create attributes height,width,length
    - create getters and setters for all attributes
    - create constructor of Box with three parameters (height,width,length)
    -  create constructor of Box with one parameters (side)

---
+++ com/lq/lab08/Box.java
@@ -0,0 +1,50 @@
+package com.lq.lab08;
+
+public class Box {
+
+	private double height;
+	private double width;
+	private double length;
+	
+	public double getHeight() {
+		return height;
+	}
+	public void setHeight(double height) {
+		this.height = height;
+		
+	}
+	public double getWidth() {
+		return width;
+	}
+	public void setWidth(double width) {
+		this.width = width;
+		
+	}
+	public double getLength() {
+		return length;
+	}
+	public void setLength(double length) {
+		this.length = length;
+		
+	}
+	
+	public Box(double height,double width,double length) {
+		
+		setHeight(height);
+		setLength(length);
+		setWidth(width);
+		
+	}
+
+
+	public Box(double side) {
+		setHeight(side);
+		setLength(side);
+		setWidth(side);
+	}
+
+
+
+
+
+}
