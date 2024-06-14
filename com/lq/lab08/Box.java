9b6915a247dc5969c7edda123eb17f95ade258cc
Rashelahmed1 <rainforest2021@yahoo.com>
2024-06-14 02:21:24 -0400

    Create methods for volume, surfaceArea , and information printing
    This commit introduces the following changes:
    
    - Create method getVolume that returns volume of the Box
    - Create method getSurfaceArea that returns surfaceArea of the Box
    - Create method printBox that prints information about length,width,height,surfaceArea,and volume of the Box

--- com/lq/lab08/Box.java
+++ com/lq/lab08/Box.java
@@ -61,6 +61,37 @@ public class Box {
 		setWidth(side);
 	}
 
+	public double getVolume() {
+		double vol = getLength() * getHeight() * getLength() ;
+		return   vol ;
+	}
+
+
+	public double getSurfaceArea(){
+		int  getLength;
+		int getWidth;
+		int getHeight;
+		double surface =  2 * getLength() * getWidth() + 2 * getHeight() * getWidth() + 2 * getLength() * getHeight() ;
+		return surface ;
+	}
+
+
+	public void PrintBox() {
+		if(getLength() <= 0 || getHeight() <= 0 || getWidth() <= 0) {
+			 System.out.println("box contain invalid properties");
+		}
+		else {
+			
+			System.out.println("Length = " + getLength());
+			System.out.println("Width = " + getWidth());
+			System.out.println("Heigth = " + getHeight());
+			System.out.println("Surface Ares = " + getSurfaceArea());
+			System.out.println("Volume = "+getVolume());
+		}
+	
+	
+	}
+
 
 
 
