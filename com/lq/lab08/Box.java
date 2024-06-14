f763f42bb0606ec7b601e9713d4646a237e97e02
Rashelahmed1 <rainforest2021@yahoo.com>
2024-06-14 02:11:26 -0400

    add validation for all setters
    This commit introduces the following changes:
    
    - Add validation for setHeight so that value less than or equal to 0 is not accepted
    - Add validation for setWidth so that value less than or equal to 0 is not accepted
    - Add validation for setLength so that value less than or equal to 0 is not accepted

--- com/lq/lab08/Box.java
+++ com/lq/lab08/Box.java
@@ -10,21 +10,39 @@ public class Box {
 		return height;
 	}
 	public void setHeight(double height) {
-		this.height = height;
+		if(height <= 0 ) {
+			System.out.println("Height must be greater than zero");
+			this.height = 1 ;
+		}
+		else {
+			this.height = height;
+		}
 		
 	}
 	public double getWidth() {
 		return width;
 	}
 	public void setWidth(double width) {
-		this.width = width;
+		if(width <= 0) {
+			System.out.println("Width must be getter than zero");
+			this.width = 1;
+		}
+		else {
+			this.width = width;
+		}
 		
 	}
 	public double getLength() {
 		return length;
 	}
 	public void setLength(double length) {
-		this.length = length;
+		if(length <= 0) {
+			System.out.println("Length must be greather than zero");
+			this.length = 1 ;
+		}
+		else {
+			this.length = length;
+		}
 		
 	}
 	
