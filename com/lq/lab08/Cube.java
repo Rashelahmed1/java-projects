84c990081b397d2bf0f07d3d4bba61911c5520a6
Rashelahmed1 <rainforest2021@yahoo.com>
2024-06-14 02:44:06 -0400

    Add validation for setters and override getters and setters
    This commit introduces the following changes:
    
    - Add validation for setSide so that value less than or equal to 0 is not accepted
    - Override getHeight so that it returns getSide
    - Override setHeight so that it sets all setters to same value
    - Override getWidth so that it returns getSide
    - Override setWidth so that it sets all setters to same value
    - Override getLength so that it returns getSide
    - Override setLength so that it sets all setters to same value

--- com/lq/lab08/Cube.java
+++ com/lq/lab08/Cube.java
@@ -9,13 +9,62 @@ public class Cube extends Box{
 	}
 
 	public void setSide(double side) {
-		this.side = side;
-			}
+		if(side <= 0) {
+			System.out.println("side must be greater than zero");
+			this.side =1 ;
+		}
+		else {
+			this.side = side;
+		}
+		
+	}
 
 	public Cube(double side) {
 		
 		super (side,side,side);
 		setSide(side);
+	}
+
+	@Override
+	public double getHeight() {
+		// TODO Auto-generated method stub
+		return getSide();
+	}
+
+	@Override
+	public void setHeight(double side) {
+		// TODO Auto-generated method stub
+		super.setHeight(side);
+		super.setLength(side);
+		super.setWidth(side);
+	}
+
+	@Override
+	public double getWidth() {
+		// TODO Auto-generated method stub
+		return getSide();
+	}
+
+	@Override
+	public void setWidth(double side) {
+		// TODO Auto-generated method stub
+		super.setWidth(side);
+		super .setLength(side);
+		super.setHeight(side);
+	}
+
+	@Override
+	public double getLength() {
+		// TODO Auto-generated method stub
+		return getSide();
+	}
+
+	@Override
+	public void setLength(double side) {
+		// TODO Auto-generated method stub
+		super.setLength(side);
+		super.setWidth(side);
+		super.setHeight(side);
 	}
 
 
