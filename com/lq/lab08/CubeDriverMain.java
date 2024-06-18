360b3de0e46608516932f2e8ac1b7a66e4b12565
Rashelahmed1 <rainforest2021@yahoo.com>
2024-06-18 10:17:08 -0400

    Creating Class CubeDriverMain
    This commit introduces the following changes:
    
    - Create object cube1 from Cube Class
    - Print cube1 information about Height,Length,Side,SurfaceArea,Volume,Width

---
+++ com/lq/lab08/CubeDriverMain.java
@@ -0,0 +1,21 @@
+package com.lq.lab08;
+
+public class CubeDriverMain {
+
+	public static void main(String[] args) {
+		Cube cube1 = new Cube(5);
+		
+		System.out.println(cube1.getHeight());
+		System.out.println(cube1.getLength());
+		System.out.println(cube1.getSide());
+		System.out.println(cube1.getSurfaceArea());
+		System.out.println(cube1.getVolume());
+		System.out.println(cube1.getWidth());
+		System.out.println("--------------");
+		
+	
+	
+	
+	}
+
+}
