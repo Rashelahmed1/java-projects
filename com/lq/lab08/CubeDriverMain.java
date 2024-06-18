e6a3c49adb3ea95388d9347efd8ff3b487961b9d
Rashelahmed1 <rainforest2021@yahoo.com>
2024-06-18 10:31:58 -0400

    Use setters on cube1 and cube2 objects
    This commit introduces the following changes:
    
    - setLength for cube1 and print all informations with getters
    - setSide for cube2 and print all informations with getters
    - setWidth invalid value  for cube2 and print all informations with printBox

--- com/lq/lab08/CubeDriverMain.java
+++ com/lq/lab08/CubeDriverMain.java
@@ -21,6 +21,29 @@ public class CubeDriverMain {
 		System.out.println(cube2.getVolume());
 		System.out.println(cube2.getWidth());
 		System.out.println("--------------");
+		
+		cube1.setLength(20);
+		
+		System.out.println(cube1.getLength());
+		System.out.println(cube1.getHeight());
+		System.out.println(cube1.getWidth());
+		System.out.println(cube1.getSide());
+		System.out.println(cube1.getSurfaceArea());
+		System.out.println(cube1.getVolume());
+		
+		cube2.setSide(40);
+		System.out.println(cube2.getLength());
+		System.out.println(cube2.getHeight());
+		System.out.println(cube2.getWidth());
+		System.out.println(cube2.getSide());
+		System.out.println(cube2.getSurfaceArea());
+		System.out.println(cube2.getVolume());
+		System.out.println("--------------");
+		
+		
+		cube2.setWidth(-5);
+		cube2.PrintBox();
+		
 	
 	}
 
