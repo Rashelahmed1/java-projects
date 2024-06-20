36c917213a3d3ce218211487701fac63a6a43b8a
Rashelahmed1 <rainforest2021@yahoo.com>
2024-06-20 10:18:35 -0400

    Create Class CD that extends Item
    This commit introduces the following changes:
    
    - Create attributes artist and releaseDate
    - Create getter and setter for all attributes

---
+++ com/lq/lab09/CD.java
@@ -0,0 +1,27 @@
+package com.lq.lab09;
+
+import java.util.Date;
+
+public class CD extends Item{
+
+	private String artist;
+	private Date releaseDate;
+	public String getArtist() {
+		return artist;
+	}
+	public void setArtist(String artist) {
+		this.artist = artist;
+	}
+	public Date getReleaseDate() {
+		return releaseDate;
+	}
+	public void setReleaseDate(Date releaseDate) {
+		this.releaseDate = releaseDate;
+	}
+
+	
+
+
+
+
+}
