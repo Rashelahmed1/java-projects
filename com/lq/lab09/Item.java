880ff7359a46f572943d2df8fa2fca7a35d87194
Rashelahmed1 <rainforest2021@yahoo.com>
2024-06-20 10:01:42 -0400

    Create Class Item
    This commit introduces the following changes:
    
    - Create attribute title,price,qty
    - Create getter and setter for all attributes

---
+++ com/lq/lab09/Item.java
@@ -0,0 +1,28 @@
+package com.lq.lab09;
+
+public class Item {
+
+	private String title;
+	private double price;
+	private int qty;
+	
+	public String getTitle() {
+		return title;
+	}
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	public double getPrice() {
+		return price;
+	}
+	public void setPrice(double price) {
+		this.price = price;
+	}
+	public int getQty() {
+		return qty;
+	}
+	public void setQty(int qty) {
+		this.qty = qty;
+	}
+	
+}
