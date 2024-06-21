161fa96e01bb58432a15856148a8e547e1441d7b
Rashelahmed1 <rainforest2021@yahoo.com>
2024-06-21 02:39:11 -0400

    Create Artist1 Class
    This commit introduces the following changes:
    
    - Create attribute name
    - Create String arrays memberNames, memberInstruments
    - Create getter and setter for name attribute

---
+++ com/lq/lab09/Artist1.java
@@ -0,0 +1,17 @@
+package com.lq.lab09;
+
+public class Artist1 {
+
+	private String name;
+	
+	String memberNames [] = new String [20] ;
+	String memberInstruments [] = new String [20];
+	
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+}
