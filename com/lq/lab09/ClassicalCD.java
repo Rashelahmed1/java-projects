8932eafa7cac940800d9a634586ca0b787b4fb9e
Rashelahmed1 <rainforest2021@yahoo.com>
2024-06-21 02:24:28 -0400

    Create ClassicalCD extends Item
    This commit introduces the following changes:
    
    - Create attributes composer,recordingLocation,releseDate
    - Create getters and setters for all attributes

---
+++ com/lq/lab09/ClassicalCD.java
@@ -0,0 +1,52 @@
+package com.lq.lab09;
+
+import java.util.Date;
+
+public class ClassicalCD extends Item{
+
+	private String composer;
+	private String recordingLocation;
+	private Date releseDate;
+
+	public String getComposer() {
+		return composer;
+	}
+
+	public void setComposer(String composer) {
+		this.composer = composer;
+	}
+
+	public String getRecordingLocation() {
+		return recordingLocation;
+	}
+
+	public void setRecordingLocation(String recordingLocation) {
+		this.recordingLocation = recordingLocation;
+	}
+
+	public Date getReleseDate() {
+		return releseDate;
+	}
+
+	public void setReleseDate(Date releseDate) {
+		this.releseDate = releseDate;
+	}
+	
+	
+	
+	
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
