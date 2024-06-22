10c7fb298f3fb5c392bfdce1d3b937850625ae4c
Rashelahmed1 <rainforest2021@yahoo.com>
2024-06-22 08:13:15 -0400

    Creating Artist Class
    This commit introduces the following changes:
    
    - Creating String attributes name, SortedSet memberName, Map memberInstruments
    - Creating getter and setter for the name

---
+++ com/lq/lab10/Artist.java
@@ -0,0 +1,52 @@
+package com.lq.lab10;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.SortedSet;
+import java.util.TreeSet;
+
+public class Artist {
+
+	private String name;
+	private SortedSet<String> memberName  ;
+	private Map<String, SortedSet<String>>  memberInstruments ;
+	
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	
+
+	
+
+	
+	 
+    
+	
+	
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
