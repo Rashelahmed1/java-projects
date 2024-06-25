d1af0cd2a48e68f71c934f2338868235ccd6d3ba
Rashelahmed1 <rainforest2021@yahoo.com>
2024-06-25 08:08:05 -0400

    Creating Item Class
    This commit introduces the following changes:
    
    - Creating attributes title,price,qty
    - Creating getters and setters for all attributes
    - Creating constructor using setters

---
+++ com/lq/lab10/Item.java
@@ -0,0 +1,41 @@
+package com.lq.lab10;
+
+public class Item {
+
+	private String title;
+	private double price;
+	private int qty;
+	
+	public Item(String title,double price,int qty) {
+		setTitle(title); 
+		setPrice(price);
+		setQty(qty);
+		
+		
+	}
+	
+	public String getTitle() {
+		return title;
+	}
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	public double getPrice() {
+		return price;
+	}
+	public void setPrice(double price) {
+		this.price = price;
+	}
+	public int getQty() {
+		return qty;
+	}
+	public void setQty(int qty) {
+		this.qty = qty;
+	}
+	
+
+
+
+
+
+}
