7038b8174daca989184dd12fbda2fa9c49d5e2ca
Rashelahmed1 <rainforest2021@yahoo.com>
2024-06-25 08:17:39 -0400

    Creating Book Class that extends Item Class
    This commit introduces the following changes:
    
    - Creating attrinbutes author,publisher,category
    - Creating getters and setters for all attributes
    - Creating constructor using setters for attributes author,publisher,category and use super constructor for attributes title,price,qty

---
+++ com/lq/lab10/Book.java
@@ -0,0 +1,34 @@
+package com.lq.lab10;
+
+public class Book extends Item{
+
+	private String author;
+	private String publisher;
+	private String category;
+	public Book(String title,double price,int qty,String author,String publisher,String category) {
+		super(title,price,qty);
+		setAuthor(author);
+		setPublisher(publisher);
+		setCategory(category);
+	}
+	
+	public String getAuthor() {
+		return author;
+	}
+	public void setAuthor(String author) {
+		this.author = author;
+	}
+	public String getPublisher() {
+		return publisher;
+	}
+	public void setPublisher(String publisher) {
+		this.publisher = publisher;
+	}
+	public String getCategory() {
+		return category;
+	}
+	public void setCategory(String category) {
+		this.category = category;
+	}
+	
+}
