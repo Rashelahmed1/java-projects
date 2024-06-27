14e9a39aaf7e1a86f6a672a56272a297b3b63b14
Rashelahmed1 <rainforest2021@yahoo.com>
2024-06-27 08:37:31 -0400

    Create InventoryDriverMain Class
    This commit introduces the following changes:
    
    - Create myInventory item of type Item
    - Create five book objects
    - Create three CD objects

---
+++ com/lq/lab10/InventoryDriverMain.java
@@ -0,0 +1,28 @@
+package com.lq.lab10;
+
+import java.util.Date;
+
+public class InventoryDriverMain {
+
+	public static void main(String[] args) {
+		Item myInventory [] = new Item [50];
+
+		Book b1 = new Book("Bangla",2.5,1,"Anis","Shaba","Text");
+		Book b2 = new Book("English",3.5,1,"Asif","Parash","Text");
+		Book b3 = new Book("Science",4.5,1,"Mustaq","Prothoma","Text");
+		Book b4 = new Book("Grammar",5.5,1,"Tisha","Book house","Text");
+		Book b5 = new Book("Mathematics",2.5,1,"Alam","Student ways","Text");
+		
+	
+	CD c1 = new CD("Story",3.2,2,"Suporna",new Date ("02/26/2024") );
+	CD c2 = new CD("History",5.2,2,"Rahim",new Date ("02/25/2024") );
+	CD c3 = new CD("Movie",6.2,2,"Karim",new Date ("02/24/2024") );
+	
+	
+	
+	
+	
+	
+	}
+
+}
