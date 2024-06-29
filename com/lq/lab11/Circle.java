b01aeb7153e63efc3f6aefc2cffde1c6fc30cd69
Rashelahmed1 <rainforest2021@yahoo.com>
2024-06-29 08:35:16 -0400

    Create Circle Class
    This commit introduces the following changes:
    
    - Create attribute radious
    - Create getter and setter for radious

---
+++ com/lq/lab11/Circle.java
@@ -0,0 +1,22 @@
+package com.lq.lab11;
+
+public class Circle {
+	
+	private double radious;
+	
+	public double getRadious() {
+		return radious;
+	}
+
+	public void setRadious(double radious) {
+		this.radious = radious;
+	}
+
+
+	
+	
+
+
+
+
+}
