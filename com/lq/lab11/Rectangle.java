5bccba0a50b145cd92c3cbf7b407cd84179aa026
Rashelahmed1 <rainforest2021@yahoo.com>
2024-07-01 08:31:20 -0400

    Creating Class Rectangle
    This commit introduces the following changes:
    
    - Creating attributes length,width
    - Creating getter and setter for all attributes

---
+++ com/lq/lab11/Rectangle.java
@@ -0,0 +1,40 @@
+package com.lq.lab11;
+
+public class Rectangle {
+
+	private double length;
+	private double width;
+	
+	
+	public double getLength() {
+		return length;
+	}
+	public void setLength(double length) {
+		this.length = length;
+	}
+	public double getWidth() {
+		return width;
+	}
+	public void setWidth(double width) {
+		this.width = width;
+	}
+	
+
+	
+	
+
+	
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
