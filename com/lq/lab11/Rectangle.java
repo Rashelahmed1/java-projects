5d247848a461e73e643d05802609f5b8555640f8
Rashelahmed1 <rainforest2021@yahoo.com>
2024-07-01 08:38:49 -0400

    Extending Shape Class in Rectangle Class
    This commit introduces the following changes:
    
    - Create constructor for Ractangle with setter of attributes length and width
    - Create constructor for Ractangle with setter of attributes length, width, name, and color.

--- com/lq/lab11/Rectangle.java
+++ com/lq/lab11/Rectangle.java
@@ -1,10 +1,21 @@
 package com.lq.lab11;
 
-public class Rectangle {
+public class Rectangle extends Shape {
 
 	private double length;
 	private double width;
-	
+	public Rectangle(double length,double width) {
+		super.setColor("White");
+		super.setName("Unknown");
+		setLength(length);
+		setWidth(width);
+		}
+	public Rectangle(double length,double width,String name,String color) {
+		super.setColor(color);
+		super.setName(name);
+		setLength(length);
+		setWidth(width);
+	}
 	
 	public double getLength() {
 		return length;
@@ -20,6 +31,9 @@ public class Rectangle {
 	}
 	
 
+	
+	
+	
 	
 	
 
