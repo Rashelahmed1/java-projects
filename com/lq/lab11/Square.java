89934d22556dd5f930b81245ddfb6ca9c41282ca
Rashelahmed1 <rainforest2021@yahoo.com>
2024-07-02 08:11:25 -0400

    Creating Square Class that extends Rectangle
    This commit introduces the following changes:
    
    - Creating constructor for Square
    - Override toString to print information about length, width, area, perimeter, color, name.

---
+++ com/lq/lab11/Square.java
@@ -0,0 +1,28 @@
+package com.lq.lab11;
+
+public class Square extends Rectangle{
+
+	public Square(double length) {
+		super(length, length);
+		
+	}
+
+	@Override
+	public String toString() {
+		return "Square [getLength()=" + getLength() + ", getWidth()=" + getWidth() + ", getArea()=" + getArea()
+				+ ", getPerimeter()=" + getPerimeter() + ", getColor()=" + getColor() + ", getName()=" + getName()
+				+ "]";
+	}
+
+	
+
+	
+
+
+
+	
+
+	
+
+
+}
