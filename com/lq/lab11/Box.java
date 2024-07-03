a2caee5c08eaa34fa131364dced8f1eec843ebde
Rashelahmed1 <rainforest2021@yahoo.com>
2024-07-03 08:19:02 -0400

    Implementing ThreeDimentional interface inside Box Class
    This commit introduces the following changes:
    
    - Implementing getVolume method
    - Implementing getSurfaceArea method
    - Creating PrintBox method that prints information about length, width, height, surfaceArea, volume
    - override toString method to print information about length, width, height, surfaceArea, volume, color, name.

--- com/lq/lab11/Box.java
+++ com/lq/lab11/Box.java
@@ -1,6 +1,6 @@
 package com.lq.lab11;
 
-public class Box extends Shape{
+public class Box extends Shape implements ThreeDimensional{
 
 	private double height;
 	private double width;
@@ -62,7 +62,44 @@ public class Box extends Shape{
 	}
 
 
+	public double getVolume() {
+		double vol = getLength() * getHeight() * getLength() ;
+		return   vol ;
+	}
 
+
+	public double getSurfaceArea(){
+		int  getLength;
+		int getWidth;
+		int getHeight;
+		double surface =  2 * getLength() * getWidth() + 2 * getHeight() * getWidth() + 2 * getLength() * getHeight() ;
+		return surface ;
+	}
+
+
+	public void PrintBox() {
+		if(getLength() <= 0 || getHeight() <= 0 || getWidth() <= 0) {
+			 System.out.println("box contain invalid properties");
+		}
+		else {
+			
+			System.out.println("Length = " + getLength());
+			System.out.println("Width = " + getWidth());
+			System.out.println("Heigth = " + getHeight());
+			System.out.println("Surface Ares = " + getSurfaceArea());
+			System.out.println("Volume = "+getVolume());
+		}
+	
+	
+	}
+	@Override
+	public String toString() {
+		return "Box [getHeight()=" + getHeight() + ", getWidth()=" + getWidth() + ", getLength()=" + getLength()
+				+ ", getVolume()=" + getVolume() + ", getSurfaceArea()=" + getSurfaceArea() + ", getColor()="
+				+ getColor() + ", getName()=" + getName() + "]";
+	}
+	
+	
 
 
 	
