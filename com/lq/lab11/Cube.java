32060793d5eaf8c10b9668faac86d2d0085802b5
Rashelahmed1 <rainforest2021@yahoo.com>
2024-07-03 08:26:57 -0400

    Create Cube Class
    This commit introduces the following changes:
    
    - Create attribute side
    - Create getter for side
    - Create constructor Cube that uses setSide to set value

---
+++ com/lq/lab11/Cube.java
@@ -0,0 +1,44 @@
+package com.lq.lab11;
+
+public class Cube {
+	
+	private double side;
+	
+	public double getSide() {
+		return side;
+	}
+
+	public void setSide(double side) {
+		
+			this.side = side;
+		
+		
+	}
+
+	public Cube(double side) {
+		
+		
+		setSide(side);
+	}
+
+	
+
+	
+
+
+	
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
