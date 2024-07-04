0bfd79f5f8db75ea064a146c8b5d0ac612ecde79
Rashelahmed1 <rainforest2021@yahoo.com>
2024-07-04 08:18:55 -0400

    using and validiting cube object inside CubeDriverMain Class
    This commit introduces the following changes:
    
    - Set the length of cube1 object as 20
    - Print cube1 object information about length, heigth, width, side, surfaceArea, volume.
    - Set the length of cube2 object as 40
    - Print cube2 object information about length, heigth, width, side, surfaceArea, volume.
    Set invalid width value of cube2 as -5
    - Use printBox method to Print cube2 object information about length, heigth, width, side, surfaceArea, volume, color, name

--- com/lq/lab11/CubeDriverMain.java
+++ com/lq/lab11/CubeDriverMain.java
@@ -3,8 +3,8 @@ package com.lq.lab11;
 public class CubeDriverMain {
 
 	public static void main(String[] args) {
+		
 		Cube cube1 = new Cube(5);
-				
 		System.out.println(cube1.getHeight());
 		System.out.println(cube1.getLength());
 		System.out.println(cube1.getSide());
@@ -20,8 +20,29 @@ public class CubeDriverMain {
 		System.out.println(cube2.getSurfaceArea());
 		System.out.println(cube2.getVolume());
 		System.out.println(cube2.getWidth());
-		System.out.println();
+		System.out.println("--------------");
 	
+		cube1.setLength(20);
+		System.out.println(cube1.getLength());
+		System.out.println(cube1.getHeight());
+		System.out.println(cube1.getWidth());
+		System.out.println(cube1.getSide());
+		System.out.println(cube1.getSurfaceArea());
+		System.out.println(cube1.getVolume());
+		System.out.println("--------------");
+	
+		cube2.setSide(40);
+		System.out.println(cube2.getLength());
+		System.out.println(cube2.getHeight());
+		System.out.println(cube2.getWidth());
+		System.out.println(cube2.getSide());
+		System.out.println(cube2.getSurfaceArea());
+		System.out.println(cube2.getVolume());
+		System.out.println("--------------");
+	
+	
+		cube2.setWidth(-5);
+		cube2.PrintBox();
 	
 	
 	
