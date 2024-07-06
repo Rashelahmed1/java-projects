f9020e2f326ccb468a8964249ba4cf39a7f228ed
Rashelahmed1 <rainforest2021@yahoo.com>
2024-07-06 08:15:28 -0400

    Creating ExercisePrint Class
    This commit introduces the following changes:
    
    - Create shape array of size 5
    - Creating Rectangle, Square, Cube, Box, Circle.

---
+++ com/lq/lab12/ExercisePrint.java
@@ -0,0 +1,27 @@
+package com.lq.lab12;
+
+import com.lq.lab11.Box;
+import com.lq.lab11.Cube;
+import com.lq.lab11.Circle;
+import com.lq.lab11.Rectangle;
+import com.lq.lab11.Shape;
+import com.lq.lab11.Square;
+
+public class ExercisePrint {
+
+	public static void main(String[] args) {
+		
+		Shape s[] = new Shape[5];
+		
+		Shape r = new Rectangle(2.2,2.3,"Rose","Purple");
+		Shape s1 = new Square(4.5);
+		Shape c1 = new Cube(4.6);
+		Shape b = new Box(5.6,5.7,5.8);
+		Shape c2 = new Circle(6.8);
+
+	
+	
+	
+	}
+
+}
