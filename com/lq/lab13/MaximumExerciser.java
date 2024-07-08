e63c26f39239e9046259ff4a7e0f58dee74cd13c
Rashelahmed1 <rainforest2021@yahoo.com>
2024-07-08 08:22:35 -0400

    Creating MaximumExerciser Class
    This commit introduces the following changes:
    
    - Use maximum static method of Maximum Class to compare three integer values
    - Use maximum static method of Maximum Class to compare three double  values
    - Use maximum static method of Maximum Class to compare three charecter values

---
+++ com/lq/lab13/MaximumExerciser.java
@@ -0,0 +1,10 @@
+package com.lq.lab13;
+
+public class MaximumExerciser {
+
+	public static void main(String[] args) {
+		System.out.println(Maximum.maximum(1, 2, 3));
+		System.out.println(Maximum.maximum(1.2, 2.2, 3.5));
+		System.out.println(Maximum.maximum("A", "B", "C"));
+	}
+}
