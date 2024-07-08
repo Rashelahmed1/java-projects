71799fe0a4ace150c871ce0d8e2202b84290c439
Rashelahmed1 <rainforest2021@yahoo.com>
2024-07-08 08:10:20 -0400

    Creating GenericMethodExerciser Class
    This commit introduces the following changes:
    
    - Creating array of integers
    - Creating array of double
    - Creating array of Characters
    - Use printArray method of GerericMethod Class to print all array items

---
+++ com/lq/lab13/GenericMethodExerciser.java
@@ -0,0 +1,29 @@
+package com.lq.lab13;
+
+public class GenericMethodExerciser {
+
+	public static void main(String[] args) {
+		
+		Integer in [] = {1,2,3};
+		Double d [] = {2.3,2.4,2.5};
+		Character c [] = {'a','b','c','d'};
+		
+	
+	GenericMethod.printArray(in);
+	GenericMethod.printArray(d);
+	GenericMethod.printArray(c);
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	}
+}
