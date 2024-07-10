40622c334cd7a37f151918818f0a561c623fb8ec
Rashelahmed1 <rainforest2021@yahoo.com>
2024-07-10 08:24:44 -0400

    Create Custom Exception class TooHotException
    This commit introduces the following changes:
    
    - Create Class TooHotException extends Exception
    - Create default constructor for TooHotException
    - Create constructor for TooHotException that accepts one perameter

---
+++ com/lq/lab14/TooHotException.java
@@ -0,0 +1,34 @@
+package com.lq.lab14;
+
+public class TooHotException extends Exception{
+
+	public TooHotException() {
+		super();
+		
+	}
+
+	public TooHotException(String message) {
+		super(message);
+		
+	}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
