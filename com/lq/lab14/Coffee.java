e2a78dc10fe6f8dcceccc59473711613b23e360e
Rashelahmed1 <rainforest2021@yahoo.com>
2024-07-10 08:32:32 -0400

    Creating Class Coffee
    This commit introduces the following changes:
    
    - Creating attribute temperature
    - Creating getter and setter for the attribute
    - Creating constructor for Coffee Class that accepts one perameter.

---
+++ com/lq/lab14/Coffee.java
@@ -0,0 +1,31 @@
+package com.lq.lab14;
+
+public class Coffee {
+
+	private int temperature;
+
+	public int getTemperature() {
+		return temperature;
+	}
+
+	public void setTemperature(int temperature)  {
+		
+	
+		
+			this.temperature = temperature;
+		
+	
+	}
+	
+	public Coffee(int temp)  {
+		setTemperature(temp);
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+}
