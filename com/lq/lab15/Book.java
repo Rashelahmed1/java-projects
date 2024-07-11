0993d791b59fff884de13710ef4a2ad9d32608dd
Rashelahmed1 <rainforest2021@yahoo.com>
2024-07-11 08:38:02 -0400

    Extending Item Class by Book Class
    This commit introduces the following changes:
    
    - Create constructor for Book that accepts title, price, author, publisher, category that uses super constructor title, price, qty.
    - Create constructor for Book that accepts title, price, author  that uses super constructor title, price, qty of 1.

--- com/lq/lab15/Book.java
+++ com/lq/lab15/Book.java
@@ -1,10 +1,21 @@
 package com.lq.lab15;
 
-public class Book {
+public class Book extends Item{
 
 	private String author;
 	private String publisher;
 	private String category;
+	public Book(String title,double price,int qty,String author,String publisher,String category) {
+		super(title,price,qty);
+		setAuthor(author);
+		setPublisher(publisher);
+		setCategory(category);
+	}
+	public Book(String title,double price,String author) {
+		super(title,price, 1);
+		setAuthor(author);
+			
+	}
 	
 	public String getAuthor() {
 		return author;
