516b66de705eaed2316f07eecb5263a75683e9fd
Rashelahmed1 <rainforest2021@yahoo.com>
2024-07-12 02:28:11 -0400

    Handle exception in BookExerciser
    This commit introduces the following changes:
    
    - Use try with resourses to read title, author, price from a given file name inside readBooksFromFile method.
    - Store extracted title, autor, price in a book objects and save it in a list and return that list
    - Catch FileNotFoundException and IOException
    - Call readBooksFromFile method inside main method

--- com/lq/lab15/BookExerciser.java
+++ com/lq/lab15/BookExerciser.java
@@ -11,13 +11,34 @@ import java.util.List;
 public class BookExerciser {
 
 	public static void main(String[] args) {
-		 
+		List<Book> books = readBooksFromFile("books.txt");  
 		
 		
 	}
 		public static List<Book> readBooksFromFile(String name){
 			List<Book> books = new ArrayList<>();
+			try 
+				(FileInputStream fin  = new FileInputStream(name);
+				InputStreamReader in_strm = new InputStreamReader(fin);
+				LineNumberReader lnr = new LineNumberReader(in_strm);)
+				{
+				String line = lnr.readLine();
+				while(line != null) {
+					String title = line;
+					String author = lnr.readLine();
+					double price = Double.parseDouble(lnr.readLine());
+					Book x = new Book(title,price,author);
+					books.add(x);
+				}
+			}
 			
+			catch (FileNotFoundException e) {
+				
+				e.printStackTrace();
+			} catch (IOException e1) {
+				
+				e1.printStackTrace();
+			} 
 			
 			
 			return books;
