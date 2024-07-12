5692e8641d591285b5b759878a8daef32ef6c596
Rashelahmed1 <rainforest2021@yahoo.com>
2024-07-12 02:18:48 -0400

    Create BookExerciser Class
    This commit introduces the following changes:
    
    - Create static method readBooksFromFile that accepts 1 perameter and returns a list of books.

---
+++ com/lq/lab15/BookExerciser.java
@@ -0,0 +1,36 @@
+package com.lq.lab15;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.LineNumberReader;
+import java.util.ArrayList;
+import java.util.List;
+
+public class BookExerciser {
+
+	public static void main(String[] args) {
+		 
+		
+		
+	}
+		public static List<Book> readBooksFromFile(String name){
+			List<Book> books = new ArrayList<>();
+			
+			
+			
+			return books;
+			
+		}
+		
+		 
+		
+		
+		
+		
+		
+
+	
+
+}
