32fa6c51ffa1f31d275a742a7012e61c9d12dac4
Rashelahmed1 <rainforest2021@yahoo.com>
2024-07-13 10:24:36 -0400

    Creating annotation MyAnnotation
    This commit introduces the following changes:
    
    - Define annotation targets as FIELD and TYPE
    - Define annotionation retentionPolicy as RUNTIPE
    - Create fields id and name

---
+++ com/lq/lab17/MyAnnotation.java
@@ -0,0 +1,15 @@
+package com.lq.lab17;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.*;
+
+@Target({ElementType.FIELD, ElementType.TYPE})
+@Retention(RetentionPolicy.RUNTIME)
+public @interface MyAnnotation {
+
+	int id();
+	String name();
+	
+}
